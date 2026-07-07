f9069e7628633abb6102bb35ffbae902286a079c
davidlegarre <davidls920@gmail.com>
2026-07-07 19:51:25 +0200

    Implement input processing for window close event in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -29,9 +33,12 @@ int main() {
     }
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    glfwTerminate();
 
     return 0;
 }
