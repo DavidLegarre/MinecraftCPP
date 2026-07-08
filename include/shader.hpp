8cb1ededf37d7e7d7e9bf0168c9327bed06d4217
davidlegarre <davidls920@gmail.com>
2026-07-08 09:02:17 +0200

    Remove unused shader uniform setter methods from Shader class

--- include/shader.hpp
+++ include/shader.hpp
@@ -9,10 +9,6 @@ class Shader {
 
     void use() const;
 
-    void setBool(const std::string& name, bool value) const;
-    void setInt(const std::string& name, int value) const;
-    void setFloat(const std::string& name, float value) const;
-
    private:
     void checkCompileErrors(unsigned int shader, std::string type);
 };
