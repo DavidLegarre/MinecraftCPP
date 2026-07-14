352e0a3e72657da31f97d9e512bd0f6084759aac
davidlegarre <davidls920@gmail.com>
2026-07-14 13:26:33 +0200

    Add Vulkan application info structure to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <vulkan/vulkan.h>
 
 int main() {
+    VkApplicationInfo appInfo{.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+                              .pApplicationName = "How to Vulkan",
+                              .apiVersion = VK_API_VERSION_1_3};
     printf("Hello, World!\n");
     return 0;
 }
